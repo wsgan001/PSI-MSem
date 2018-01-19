f986d465c11f617bf7740c38bc18a30ce8e1e853
EisbergSalat <tollpatsch93@gmail.com>
2018-01-19 23:30:06 +0100

    Task2 Part1 solved

--- weknowwhoyouare.py
+++ weknowwhoyouare.py
@@ -1,24 +1,22 @@
 import numpy as np
+from progress.bar import Bar
 
 delay = 10
 students = set()
-left = [[],[]]
-#students.append([])
-right = [[],[]]
+left = [[], []]
+right = [[], []]
 server = set()
-#server.append([])
-
 
 
 def count_students():
     with open("left.txt", 'r') as f:
-        i = 0
         for line in f.readlines():
             words = line.lower().split()
             left.append(words[0])
-            students.add(words[0])           
-    print(len(students))
-    #print(students)
+            students.add(words[0])
+    print("count of students> ", len(students))
+    # print(students)
+
 
 def count_server():
     with open("right.txt", 'r') as f:
@@ -26,17 +24,30 @@ def count_server():
             words = line.lower().split()
             right.append(words[2])
             server.add(words[2])
-    print(len(server))
-    #print(server)
+    print("count of server> ", len(server))
+    # print(server)
+
 
 def top5servers():
-    servercount = [[]]
+    if len(server) == 0:
+        count_server()
+    servercount = []
     listofserver = list(server)
-    for i in range(0, len(server)):
-        servercount.append([listofserver[i],right.count(listofserver[i])])
-    print(servercount)
-    
-count_server()
+    bar = Bar('Processing', max=len(server))
+    for i in (range(0, len(server))):
+        bar.next()
+        servercount.append([listofserver[i], right.count(listofserver[i])])
+        servercount.sort(reverse=True, key=takeSecond)
+
+    bar.finish()
+    print(servercount[:5], "\n")
+
+
+def takeSecond(elem):
+    return elem[1]
+
+
+# count_server()
+# top5servers()
 count_students()
-top5servers()
 print("done")
